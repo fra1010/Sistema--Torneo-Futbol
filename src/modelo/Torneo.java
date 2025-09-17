6b1ce8bb65d4dc3cda532a5611116ac6a5d4c663
fra1010 <142928058+fra1010@users.noreply.github.com>
2025-09-17 18:31:15 -0300

    Add files via upload

--- src/modelo/Torneo.java
+++ src/modelo/Torneo.java
@@ -13,6 +13,66 @@ public class Torneo {
     private LocalDate fechaInicio;
     private LocalDate fechaFin;
     private List<Partido> partidos;
+    
+    
+	public Torneo(int idTorneo, String nombre, String temporada, List<Equipo> equipos, LocalDate fechaInicio,
+			LocalDate fechaFin, List<Partido> partidos) {
+		super();
+		this.idTorneo = idTorneo;
+		this.nombre = nombre;
+		this.temporada = temporada;
+		this.equipos = new ArrayList<Equipo>();
+		this.fechaInicio = fechaInicio;
+		this.fechaFin = fechaFin;
+		this.partidos = new ArrayList<Partido>();
+	}
+	@Override
+	public String toString() {
+		return "Torneo [idTorneo=" + idTorneo + ", nombre=" + nombre + ", temporada=" + temporada + ", equipos="
+				+ equipos + ", fechaInicio=" + fechaInicio + ", fechaFin=" + fechaFin + ", partidos=" + partidos + "]";
+	}
+	public int getIdTorneo() {
+		return idTorneo;
+	}
+	public void setIdTorneo(int idTorneo) {
+		this.idTorneo = idTorneo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTemporada() {
+		return temporada;
+	}
+	public void setTemporada(String temporada) {
+		this.temporada = temporada;
+	}
+	public List<Equipo> getEquipos() {
+		return equipos;
+	}
+	public void setEquipos(List<Equipo> equipos) {
+		this.equipos = equipos;
+	}
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(LocalDate fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public LocalDate getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(LocalDate fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+	public List<Partido> getPartidos() {
+		return partidos;
+	}
+	public void setPartidos(List<Partido> partidos) {
+		this.partidos = partidos;
+	}
 
 	
 	
